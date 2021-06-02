dba40d5766b60c61b405b3a5547140d22142a3ac
Jorge Esteban Caballero Rodríguez <62854093+JECaballeroR@users.noreply.github.com>
2021-06-02 18:57:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ data=pd.DataFrame([[ "Usuario","Fecha último pago", "Tiempo de Prueba"],
                    ["Esteban",dt.date.fromisoformat('2021-03-24'), 120],
                    ["Gabriel", dt.date.fromisoformat('2021-03-04'), 30],
                    ["Jose", dt.date.fromisoformat('2021-06-01'), 15],
-                   ["Angela", dt.date.fromisoformat('2021-06-01'), 0],
+                   ["Angela", dt.date.fromisoformat('2021-06-02'), 0],
                    ])
 data.columns=data.iloc[0]
 data=data[1:]
 
 st.markdown("# Prueba de lógica de registro de usuarios")
+st.markdown("A continuación, se presenta la prueba de la lógica general de cómo manejar usuarios.")
+st.markdown("** La  tabla a continuación es editable (dar doble clic) **")
+st.markdown("En teoría, si ahora <= el día del último pago + tiempo de prueba  media noche, está activado.")
+st.markdown("Si se excede ese valor, se desactiva al usuario.")
+st.markdown("La idea es probar que la lógica funciona aplicando casos con la tabla interativa)")
+
 data=AgGrid(data, editable=True,
     sortable=True,
     filter=True,
@@ -29,7 +35,7 @@ df=df.reset_index()
 df['Fecha último pago']=pd.to_datetime(df['Fecha último pago'])
 
 st.markdown("# Estado de las personas")
-fechas=[df['Fecha último pago'][i]+dt.timedelta(days=int(df['Tiempo de Prueba'][i])) for i in range(len(df))]
+fechas=[df['Fecha último pago'][i]+dt.timedelta(days=int(df['Tiempo de Prueba'][i]), hours=23, minutes=59, seconds=59) for i in range(len(df))]
 
 c1, c2, c3 = st.beta_columns(3)
 c1.markdown("## Usuario")
@@ -39,7 +45,7 @@ desactivados=[]
 for i in range(len(df)):
     c1.write(df["Usuario"][i])
     c2.write(fechas[i])
-    if dt.datetime.now()<fechas[i]:
+    if dt.datetime.now()<=fechas[i]:
         c3.write("Activo")
     else:
         c3.write("Desactivado")
